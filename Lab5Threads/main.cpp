5c2b2cdf6b4a0af13e34735f3994bb669d5492f3
Jack Harding <jackharding321@gmail.com>
2018-10-16 20:50:36 +0100

    Lab5Threads Complete

--- Lab5Threads/main.cpp
+++ Lab5Threads/main.cpp
@@ -1,34 +1,55 @@
 #include <iostream>
+#include <string>
 #include <unistd.h>
 #include <pthread.h> /* POSIX threads */
 using namespace std;
 
-int i = 0;
+//int i = 0;
+string gMessage = "C++ is bollox";
 
-void *thread_routine1 (void *arg1 ){ // of type void pointer, accepting void pointer
+/*
+void *thread_routine1(void *arg1){ // of type void pointer, accepting void pointer
     /* Task 3
     int j;
     for(i = 0; i < 10; i++)
         for(j = 0; j < 10; j++)
            cout << "Hello, World (thread " << i << ") \n";
-           */
-    // 3A
-    int test = *((int *) arg1); // dereferencing arg as a void
-    cout <<"Hello ";
+
+    // Task 3B
+    cout << *static_cast<string*>(arg1) << endl; // static cast: converts types
     pthread_exit(NULL);
+    return nullptr;
 }
 
 void *thread_routine2 (void *arg2 ){ // second thread function
-    cout <<"world \n";
+    // Task 3B
+    cout << *static_cast<string*>(arg2) << endl; // static cast: converts types
     pthread_exit(NULL);
+    return nullptr;
+}
+*/
+void *thread_routine3 (void *arg3 ){ // asks for text to be inputted, nothing passed
+    // Task 4
+    cout << "\nPlease enter something \n";
+    cin >> gMessage;
+    cout << endl;
+    //cout << *static_cast<void*>(gMessage) << endl; // static cast: converts type
+    pthread_exit(NULL);
+    return nullptr;
+}
+
+void *thread_routine4 (void *arg4 ){ // prints the global var
+    // Task 4
+    cout << *static_cast<string*>(arg4) << endl; // static cast: converts type
+    pthread_exit(NULL);
+    return nullptr;
 }
 
 int main ()
 {
     /*
+    // Task 2
     int rc;
-    pthread_t thread1 ;
-    pthread_t thread2 ;
 
     cout <<"main process has PID="<< getpid() << endl;
     create thread
@@ -38,7 +59,7 @@ int main ()
 
 
    // rc=pthread_create( &thread1, NULL, &thread_routine1, NULL);
-   // if (rc==1) cout <<"Thread creation failed\n";
+   // if (rc==1) cout <<"Thread creation        failed\n";
     */
     /* Task 3
     pthread_t threads[10]; // array of pthreads
@@ -48,14 +69,34 @@ int main ()
     }
     pthread_join(threads[9], NULL);// suspend execution of the calling thread
        */                             // until the target thread terminates
-    //Task 3A
+    /*Task 3B
+
     pthread_t thread1;
     pthread_t thread2;
 
     string t1 = "hello", t2 = "world";
-    pthread_create(&thread1, NULL, thread_routine1, t1);
-    pthread_create(&thread2, NULL, thread_routine2, t2);
-    pthread_exit(NULL);
+    pthread_create(&thread1, NULL, thread_routine1, &t1); // pass the address of t1
+    pthread_create(&thread2, NULL, thread_routine2, &t2);
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    // Task 4
+    pthread_t thread3;
+    pthread_t thread4;
+    while(1) {// indefinite loop
+        pthread_create(&thread3, NULL, thread_routine3, NULL);
+        pthread_create(&thread4, NULL, thread_routine4, &gMessage);
+        pthread_join(thread3, NULL);
+        pthread_join(thread4, NULL);
+    }*/
+    // Task 5: same as previous task but with no join
+    pthread_t thread3;
+    pthread_t thread4;
+    while(1) {// indefinite loop
+        pthread_create(&thread3, NULL, thread_routine3, NULL);
+        pthread_create(&thread4, NULL, thread_routine4, &gMessage);
+    }
+    return 0;
 }
 
 /*
@@ -65,8 +106,7 @@ Task1:
 3. The order in which the function are printed is irregular, suggests
     two threads.
 
-Task2:
-
-
-    https://computing.llnl.gov/tutorials/pthreads/#PthreadsAPI
+Task5:
+    Prints out the same strings but in completely random order and without
+    waiting for user input.
 */
