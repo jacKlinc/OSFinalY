73c92fd94b4eaabf67fa511aae4223974569d97c
Jack Harding <jackharding321@gmail.com>
2018-11-16 15:33:29 +0000

    Organised

---
+++ parent/parent/parent.cpp
@@ -0,0 +1,18 @@
+#include "unistd.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+    int processID = fork();
+    int counter = 0;
+    if(processID == 1){
+        for(int i = 0; i < 100; i++)
+            cout << "hello from parent, Counter: %d\n", ++counter;
+
+    } else if(processID == 0){
+        for(int i = 0; i < 100; i++)
+            cout << "hello from child, Counter: %d\n", ++counter;
+            execl();
+    }
+    cout << "----End------";
+}
